c13d641a1172894c45106385beb25092eba89fb8
VMGhelani <157642044+VMGhelani@users.noreply.github.com>
2024-02-28 20:59:32 +0530

    Add Home Page design

--- ui/src/components/Home/Home.jsx
+++ ui/src/components/Home/Home.jsx
@@ -6,6 +6,7 @@ import Signup from '../Signup/Signup.jsx';
 import Login from '../Login/Login.jsx';
 import Product_Listing from '../Product_Listing/Product_Listing.jsx';
 import ListingUpload from '../listing_upload/ListingUpload.jsx';
+import ProductHome from '../Product_HomePage/index.js';
 
 const Home = () => {
     return (
@@ -17,6 +18,7 @@ const Home = () => {
                     <Route path='login' element={<Login />} />
                     <Route path='productListings' element={<Product_Listing/>} />
                     <Route path='ListingsUpload' element={<ListingUpload/>} />
+                    <Route path='home' element={<ProductHome/>} />
                 </Routes>
             </BrowserRouter>
             <Footer />
